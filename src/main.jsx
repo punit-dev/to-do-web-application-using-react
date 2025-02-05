8145988d09ba36fd61f61290b893339a00bcdeae
punit-dev <punitpoddar2310@gmail.com>
2025-02-05 14:06:07 +0530

    Rename ToDoLists context file to ToDoListsContext.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ToDoProvider } from "./context/ToDoLists.jsx";
+import { ToDoProvider } from "./context/ToDoListsContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <ToDoProvider>
